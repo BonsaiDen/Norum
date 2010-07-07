941cc10dd042a04558239e243ff454d3f6d3f4aa
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-07-07 14:38:53 +0200

    More stuff.

--- headers/macro.h
+++ headers/macro.h
@@ -1,5 +1,3 @@
-//#define EDITOR
-
 #ifndef EDITOR
     #define WINDOW_WIDTH 320
     #define WINDOW_HEIGHT 240
@@ -12,31 +10,29 @@
 #endif
 
 
+// Some stuff that's not in C89
+#define min(x1,x2) ((x1) > (x2) ? (x2):(x1))
+#define max(x1,x2) ((x1) > (x2) ? (x1):(x2))
+
+#define bool char
+#define true 1
+#define false 0
+
 #ifdef WIN32
+    #define likely(x)       (x)
+    #define unlikely(x)     (x)
     
     // Visual Studio
     #ifdef _MSC_VER
         #pragma comment(lib, "SDL.lib")
         #pragma comment(lib, "SDLmain.lib")
         #pragma comment(lib, "SDL_image.lib")
-
-        #define fmin(x1,x2) ((x1) > (x2) ? (x2):(x1))
-        #define fmax(x1,x2) ((x1) > (x2) ? (x1):(x2))
-
-        #define bool int
-        #define true 1
-        #define false 0
-    
-    // Code Blocks
-    #else
-        #include <stdbool.h>
-        #define likely(x)		(x)
-        #define unlikely(x)		(x)
     #endif
 
 // GCC Linux
 #else
-    #include <stdbool.h>
+    
+    // Branching "optimization"
     #define likely(x)       __builtin_expect((x),1)
     #define unlikely(x)     __builtin_expect((x),0)
 #endif
